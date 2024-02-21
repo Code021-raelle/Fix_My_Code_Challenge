14b879993a88d23a779544ecaceaf46889de3f92
Code021-raelle <gabrielakinshola3@gmail.com>
2024-02-21 14:18:22 +0100

    fizzbuzz

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -10,7 +10,7 @@ if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
 const size = parseInt(process.argv[2]);

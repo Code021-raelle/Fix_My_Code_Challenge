deeb56db0b73a09f7dbd433c1fceef741739f5d9
Code021-raelle <gabrielakinshola3@gmail.com>
2024-02-21 14:32:02 +0100

    fizzbuzz

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -37,7 +37,7 @@ class User():
         - `None` if `pwd` is not a string
         - Hash `pwd` in MD5 before assign to `__password`
         """
-        if pwd is None or not isinstance(pwd, str):
+        if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()

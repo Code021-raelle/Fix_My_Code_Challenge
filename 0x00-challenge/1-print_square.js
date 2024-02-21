ad6b7b182151997933864f7912f85704c6b30702
Code021-raelle <gabrielakinshola3@gmail.com>
2024-02-21 14:20:16 +0100

    fizzbuzz

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,7 +13,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-const size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10);
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {

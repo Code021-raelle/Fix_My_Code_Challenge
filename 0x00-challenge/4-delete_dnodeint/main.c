0e39a712fe6886bfe16c11aecd5798fd515603be
Code021-raelle <gabrielakinshola3@gmail.com>
2024-02-22 10:08:10 +0100

    nodeint

---
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint_end(&head, 0);
+
